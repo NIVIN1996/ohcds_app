ab4b32d89a6e2d4a6330b62cf975ca7ec811cff2
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-04-21 09:42:32 +0530

    class for all ui colors

---
+++ lib/utils/colors/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primaryColor = Color(0xff313131);
+  static const Color yellowColor = Color(0xffFFD000);
+  static const Color darkGrayColor = Color(0xff313131);
+  static const Color darkGrayBorderColor = Color(0xff212121);
+  static const Color grayColor = Color(0xff58595B);
+  static const Color orangeColor = Color(0xffE97E20);
+  static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color lightGrayColor = Color(0xffC3C3C3);
+  static const Color blackColor = Color(0xff000000);
+  static const Color shimmerColor = Color(0xFFE0E0E0);
+  static const Color mediumGrayColor = Color(0xffAAAAAA);
+  static const Color borderGrayColor = Color(0xffDEE3E9);
+  static const Color greenColor = Color(0xff00D263);
+  static const Color redColor = Color(0xFFF16073);
+  static const Color offWhite = Color.fromRGBO(221, 221, 221, 1);
+
+}
