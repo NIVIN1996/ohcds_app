4298fab5387dbffd64d43e69d88c4d46bdc99567
NIVIN1996 <nivin@ateamsoftsolutions.com>
2022-04-21 09:46:50 +0530

    image path class for all images and svg

---
+++ lib/utils/image_path/image_path.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class AppImages {
+
+  static const String menu = "assets/svg/menu.svg";
+  static const String search = "assets/svg/search.svg";
+  static const String call = "assets/svg/call.svg";
+  static const String card = "assets/svg/card.svg";
+  static const String claims = "assets/svg/claims.svg";
+  static const String favorite = "assets/svg/favorite.svg";
+  static const String myCard = "assets/svg/my_card.svg";
+  static const String notification = "assets/svg/notification.svg";
+  static const String offer = "assets/svg/offers.svg";
+  static const String order = "assets/svg/order.svg";
+  static const String profile = "assets/svg/profile.svg";
+  static const String setting = "assets/svg/setting.svg";
+  static const String tickets = "assets/svg/tickets.svg";
+  static const String signOut = "assets/svg/sign_out.svg";
+  static const String profileImage = "assets/svg/profile_img.svg";
+  static const String tiktok = "assets/svg/tiktok.svg";
+  static const String twitter = "assets/svg/twitter.svg";
+  static const String insta = "assets/svg/insta.svg";
+  static const String home = "assets/svg/home.svg";
+  static const String edit = "assets/svg/edit.svg";
+  static const String setting2 = "assets/svg/setting2.svg";
+  static const String success = "assets/svg/success.svg";
+
+  static const String logo = "assets/images/Logo2.png";
+  static const String logo2 = "assets/images/Logo.png";
+  static const String search2 = "assets/svg/search.png";
+  static const String cart2 = "assets/svg/cart2.png";
+  static const String homeImg = "assets/images/home.png";
+  static const String searchImg = "assets/images/search.png";
+  static const String favImg = "assets/images/fav.png";
+  static const String cart2Img = "assets/images/cart2.png";
+  static const String home2Img = "assets/images/home2.png";
+  static const String fav2Img = "assets/images/fav2.png";
+  static const String cartImg = "assets/images/cartImg.png";
+  static const String searchImg2 = "assets/images/searchImg.png";
+  static const String backArrow = "assets/images/back_arrow.png";
+  static const String order1 = "assets/images/order1.png";
+  static const String order2 = "assets/images/order2.png";
+  static const String order3 = "assets/images/order3.png";
+  static const String noImg = "assets/images/no_img.png";
+
+
+}
